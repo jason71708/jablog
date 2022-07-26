1258a736c4fd606cbc1955cc6620673759264af3
bo cheng <jason71708@gmail.com>
2022-07-27 01:32:40 +0800

    feat: add comment at doc and blog

---
+++ src/theme/BlogPostItem/Footer/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Footer from '@theme-original/BlogPostItem/Footer';
+import Giscus from '@giscus/react';
+
+export default function FooterWrapper(props) {
+  return (
+    <>
+      <Footer {...props} />
+      <Giscus
+        id="comments"
+        repo="jason71708/jablog"
+        repoId="R_kgDOHuA6TA"
+        category="Announcements"
+        categoryId="DIC_kwDOHuA6TM4CQcHo"
+        mapping="title"
+        // term="Welcome to @giscus/react component!"
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme="preferred_color_scheme"
+        lang="zh-TW"
+        loading="lazy"
+        crossOrigin="anonymous"
+      />
+    </>
+  );
+}
